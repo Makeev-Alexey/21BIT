80933db4477ac4365ce53212be66fc767c599c32
aleksej <allozvr-al@mail.ru>
2022-04-08 16:02:32 +0300

    Initial commit

---
+++ app/src/main/java/com/example/a21bit/ArticleActivity.kt
@@ -0,0 +1,23 @@
+package com.example.a21bit
+
+import Articles
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.text.Html
+import android.webkit.WebViewClient
+import com.example.a21bit.databinding.ActivityArticleBinding
+
+class ArticleActivity : AppCompatActivity() {
+    private var _binding : ActivityArticleBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityArticleBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        var article = intent.getParcelableExtra<Articles>("article")
+            binding.web.webViewClient = WebViewClient()
+            binding.web.loadUrl(article?.url.toString())
+            binding.web.settings.javaScriptEnabled = true
+            binding.web.settings.setSupportZoom(true)
+    }
+}
